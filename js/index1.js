7be294908c0bac6824883df2596b4ee137ea544e
cj <youngboycj@163.com>
2016-12-16 00:14:00 +0800

    2016/12/16-00:13

--- js/index1.js
+++ js/index1.js
@@ -32,57 +32,82 @@ function Ajax(appid,api,maxResult){
 		success:function(data){
 			console.log(data);
 			data = data.showapi_res_body.contentlist;
+			var _imgs = [];
+			var _titles = [];
+			for(var i=0;i<data.length;i++){
+				 _imgs.push(data[i].img);
+				 _titles.push(data[i].title);
+				
+			}
+			//console.log(_imgs,_titles);
 			//console.log(data);
 			function show(i,end){
-				for(; i<end; i++){
+				/*for(; i<end; i++){
 					str += `<li><div class="imgdiv"><img src="${data[i].img}"></div>
 					<p>${(i+1)+"."+data[i].title}</p></li>`;
 					//$("li").index = i;//用于左右滑动
+				}*/
+				for(; i<end; i++){
+					str += `<li><div class="imgdiv"><img src="${_imgs[i]}"></div>
+					<p>${(i+1)+"."+_titles[i]}</p></li>`;
 				}
 				$(".ul2").html(str);
 			}
 
 			show(0,items);//初始显示
 
-			$(".previous").on("click",function(){
+			$(".previous").on("touchstart",function(){
+				$("section").children("p.pend").remove();
 				index--;
 				if( index <= 0 ){
-					show(0,items+tiaomu);
-					console.log(index);
+					console.log(index+"<"+items+',,' + pages+ "a");
 					index = 0;
-				}else{
-					//show(items,items+tiaomu);
-					console.log(index);
+					show(0,tiaomu);
+					return false;
+				}else if(index === pages){
+					var x = maxResult - last;
+					show(x,x+last);
+					console.log(index+"==="+ items +',,' + pages+"b");
+				}else {
+					items = items - tiaomu;
+					show(items,items+tiaomu);
+					console.log(index+"---"+items +',,' + pages+"c");
 				}
-				
 			});
 
 			 
-			$(".next").on("click",function(){
+			$(".next").on("touchstart",function(){
 				index++;
-				if(index === pages){
-					console.log(items,index);
-					show(items,items+last);
-					$("section").append("<p style='width:100%;text-align:center;font-size:20px; color:red; font-family:微软雅黑'>END...</p>");
-					//console.log("相等")
-					console.log(index);
-				}else{
+				if(index < pages){
 					show(items,items+tiaomu);
 					//console.log("不等");
-					console.log(index);
+					//console.log(index);
+					console.log(index,items +',,' + pages+"d");
+				}else if(index === pages){
+					str1 = "<p class='pend' style='width:100%;text-align:center;font-size:20px; color:red; font-family:微软雅黑'>END...</p>";
+					show(items,items+last);
+					$("section").append(str1);
+					//console.log("相等")
+					//console.log(index);
+					console.log(index,items+',,' + pages+"e");
+					index
+					return false;
+				}else{
+					console.log(index,items+',,' + pages +"f");
+					index = pages;
+					return false;
 				}
 				items = items + tiaomu;
-				
+
 			});
 
 			
-			//console.log(strid);
 			$(".ul2>li").on("touchstart",function(event){
 				//console.log($("html").width());
-				var touch = event.targetTouches[0].pageX;
+				//var touch = event.targetTouches[0].pageX;
 				//console.log(touch.pageX);
 				//console.log($(this).css("width"));
-				
+				console.log(1234456567);
 				var _fontsize = (($("html").width()*100)/640).toFixed(0)*2.5 + "px";
 				//console.log(_fontsize);
 				//console.log($(this).css("height"));
@@ -102,7 +127,7 @@ function Ajax(appid,api,maxResult){
 			$("footer .next").on("touchstart",function(){	
 
 			});
-			str1 += maxResult;
+		
 		}
 	});	
 }
