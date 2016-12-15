0859201f47a0e98dc3ca47d8355fc70d230e6af1
caijun <515734501@qq.com>
2016-12-15 12:46:37 +0800

    这是我第二次修改

--- js/index1.js
+++ js/index1.js
@@ -1,12 +1,20 @@
 $(function(){
-	$("header li").on("touchstart",function(){
-		console.log(123456);
+	var _appid = "28912";
+	var _apikey = "3b31ec8293d147848f18dffd2be7f9aa";
+	var _max = "40";
+	Ajax(_appid,_apikey,30);
+	$("header li").on("click",function(){
+		Ajax(_appid,_apikey,_max);
 	});
+	
+});
+
+function Ajax(appid,api,maxResult){
 	var strid = [];
 	var arr = [];
 	var str = "";
-	var _apikey = "3b31ec8293d147848f18dffd2be7f9aa";
-	var _url = "http://route.showapi.com/341-3?showapi_appid=	28912&showapi_sign=3b31ec8293d147848f18dffd2be7f9aa&maxResult=30" ;
+	var str1 = "";
+	var _url = "http://route.showapi.com/341-3?showapi_appid="+	appid +"&showapi_sign="+ api +"&maxResult=" + maxResult ;
 	$.ajax({
 		url: _url,
 		type:"GET",
@@ -23,10 +31,10 @@ $(function(){
 			for(var i=0,len=data.length; i<len; i++){
 				strid.push(data[i].id);//将每张图的id保存
 				str += `<li><img src="${data[i].img}">
-				<p>${data[i].title}</p></li>`;
+				<p>${(i+1)+"."+data[i].title}</p></li>`;
 				$("li").index = i;//用于左右滑动
 			}
-			$("section>ul").append(str);
+			$("section>ul").html(str);
 			//console.log(strid);
 			$(".ul2>li").on("touchstart",function(event){
 
@@ -44,9 +52,13 @@ $(function(){
 					$("footer").show();
 				}
 			});
-				$("footer").show();
-				$(".next").on("touchstart",function(){	
-				});
+				
+			$("footer").show();
+			$("footer .next").on("touchstart",function(){	
+
+			});
+			str1 += maxResult;
+			console.log(str1);
 		}
 	});	
-});
+}
