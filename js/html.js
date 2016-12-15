3edf17fe7779ad3f0808abbc0f26592ee66082b0
caijun <515734501@qq.com>
2016-12-15 20:48:25 +0800

    这是我第三次提交

---
+++ js/html.js
@@ -0,0 +1,5 @@
+var _width = $("html").width();
+//console.log(_width);
+var _fontsize = ((_width*100)/640).toFixed(0) + "px";
+console.log(_fontsize);
+$("html").css("font-size",_fontsize);
--- js/index1.js
+++ js/index1.js
@@ -1,8 +1,8 @@
 $(function(){
 	var _appid = "28912";
 	var _apikey = "3b31ec8293d147848f18dffd2be7f9aa";
-	var _max = "40";
-	Ajax(_appid,_apikey,30);
+	var _max = "50";
+	Ajax(_appid,_apikey,50);
 	$("header li").on("click",function(){
 		Ajax(_appid,_apikey,_max);
 	});
@@ -10,7 +10,11 @@ $(function(){
 });
 
 function Ajax(appid,api,maxResult){
-	var strid = [];
+	var items = 20;
+	var tiaomu = 20;
+	var pages = parseInt(maxResult/items);
+	var last = maxResult - items*pages;//用于判断最后一页需要加载多少
+	var index = 0;
 	var arr = [];
 	var str = "";
 	var str1 = "";
@@ -26,30 +30,71 @@ function Ajax(appid,api,maxResult){
 			$("ul").html("<p style='width:100%;font-size:16px;text-align:center;'>数据加载中...</p>");
 		},*/
 		success:function(data){
-			//console.log(data);
+			console.log(data);
 			data = data.showapi_res_body.contentlist;
-			for(var i=0,len=data.length; i<len; i++){
-				strid.push(data[i].id);//将每张图的id保存
-				str += `<li><img src="${data[i].img}">
-				<p>${(i+1)+"."+data[i].title}</p></li>`;
-				$("li").index = i;//用于左右滑动
+			//console.log(data);
+			function show(i,end){
+				for(; i<end; i++){
+					str += `<li><div class="imgdiv"><img src="${data[i].img}"></div>
+					<p>${(i+1)+"."+data[i].title}</p></li>`;
+					//$("li").index = i;//用于左右滑动
+				}
+				$(".ul2").html(str);
 			}
-			$("section>ul").html(str);
+
+			show(0,items);//初始显示
+
+			$(".previous").on("click",function(){
+				index--;
+				if( index <= 0 ){
+					show(0,items+tiaomu);
+					console.log(index);
+					index = 0;
+				}else{
+					//show(items,items+tiaomu);
+					console.log(index);
+				}
+				
+			});
+
+			 
+			$(".next").on("click",function(){
+				index++;
+				if(index === pages){
+					console.log(items,index);
+					show(items,items+last);
+					$("section").append("<p style='width:100%;text-align:center;font-size:20px; color:red; font-family:微软雅黑'>END...</p>");
+					//console.log("相等")
+					console.log(index);
+				}else{
+					show(items,items+tiaomu);
+					//console.log("不等");
+					console.log(index);
+				}
+				items = items + tiaomu;
+				
+			});
+
+			
 			//console.log(strid);
 			$(".ul2>li").on("touchstart",function(event){
-
+				//console.log($("html").width());
 				var touch = event.targetTouches[0].pageX;
 				//console.log(touch.pageX);
 				//console.log($(this).css("width"));
-				if($(this).css("width") === "128px"){
-					$(this).css({"width":"100%"}).siblings().hide();
+				
+				var _fontsize = (($("html").width()*100)/640).toFixed(0)*2.5 + "px";
+				//console.log(_fontsize);
+				//console.log($(this).css("height"));
+				if($(this).css("height") === _fontsize){
+					$(this).css({"width":"100%","height":"100%"}).siblings().hide();
 					$(this).find("p").css({"color":"red"});
 					$("footer").hide();
-					/*$(screen).css("height",touch);*/
 				}else{
-					$(this).css({"width":"128px"}).siblings().show();
+					$(this).css({"height":_fontsize,"width":"40%"}).siblings().show();
 					$(this).find("p").css({"color":"rgba(0,0,0,0.6)"});
 					$("footer").show();
+					//console.log(456);
 				}
 			});
 				
@@ -58,7 +103,6 @@ function Ajax(appid,api,maxResult){
 
 			});
 			str1 += maxResult;
-			console.log(str1);
 		}
 	});	
 }
